912c33041de49ff8c7f4078e2b3537801615fd8c
Kassem Bagher <kbagher@lucidya.com>
2024-06-22 20:03:15 +1000

    Enable logging and refactor debug class
    Activated previously comment out logging feature in main_client.py. In debug.py, refactored debug class to improve logging mechanism by separating console and file logging tasks into different threads. This provides finer control over log messages and enhances performance.

--- ZeroMQFramework/helpers/debug.py
+++ ZeroMQFramework/helpers/debug.py
@@ -7,7 +7,6 @@ import logging
 from enum import Enum
 from logging.handlers import QueueHandler, QueueListener
 import queue
-from contextlib import contextmanager
 
 
 class LogMode(Enum):
@@ -23,7 +22,9 @@ class Debug:
     log_folder = None
     logger = None
     log_queue = queue.Queue()
-    listener = None  # Add a class variable to hold the listener instance
+    console_queue = queue.Queue()  # Separate queue for console logging
+    listener = None
+    worker_thread = None
 
     ANSI_CODES = {
         'red': "\033[31m",
@@ -60,6 +61,10 @@ class Debug:
         cls.listener = QueueListener(cls.log_queue, handler)
         cls.listener.start()
 
+        # Start the worker thread
+        cls.worker_thread = threading.Thread(target=cls._log_worker, daemon=True)
+        cls.worker_thread.start()
+
     @classmethod
     def clean_old_logs(cls, log_folder):
         now = datetime.datetime.now()
@@ -72,18 +77,18 @@ class Debug:
 
     @classmethod
     def info(cls, *messages, mode=LogMode.NEWLINE, force=False):
-        cls._log(cls.ANSI_CODES['green'], "INFO", *messages, mode=mode, force=force)
+        cls._enqueue_log(cls.ANSI_CODES['green'], "INFO", *messages, mode=mode, force=force)
 
     @classmethod
     def warn(cls, *messages, mode=LogMode.NEWLINE, force=False):
-        cls._log(cls.ANSI_CODES['yellow'], "WARN", *messages, mode=mode, force=force)
+        cls._enqueue_log(cls.ANSI_CODES['yellow'], "WARN", *messages, mode=mode, force=force)
 
     @classmethod
     def error(cls, *messages, mode=LogMode.NEWLINE, force=False):
-        cls._log(cls.ANSI_CODES['red'], "ERROR", *messages, mode=mode, force=force)
+        cls._enqueue_log(cls.ANSI_CODES['red'], "ERROR", *messages, mode=mode, force=force)
 
     @classmethod
-    def _log(cls, color_code, log_type, *messages, mode=LogMode.NEWLINE, force=False):
+    def _enqueue_log(cls, color_code, log_type, *messages, mode=LogMode.NEWLINE, force=False):
         if cls.enabled or force:
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -92,19 +97,7 @@ class Debug:
                     message = f"{message}\n{traceback.format_exc()}"
 
                 log_message = f"[{timestamp}] {cls.prefix} {log_type}: {message}"
-
-                # Print to console with color formatting
-                if mode == LogMode.UPDATE:
-                    sys.stdout.write('\r')
-                    sys.stdout.write(f"{color_code}{log_message}{cls.ANSI_CODES['reset']}")
-                    sys.stdout.flush()
-                    cls.last_message = LogMode.UPDATE
-                elif mode == LogMode.NEWLINE:
-                    if cls.last_message == LogMode.UPDATE:
-                        sys.stdout.write("\n")
-                    sys.stdout.write(f"{color_code}{log_message}{cls.ANSI_CODES['reset']}\n")
-                    sys.stdout.flush()
-                    cls.last_message = LogMode.NEWLINE
+                cls.console_queue.put((color_code, log_type, log_message, mode))
 
                 # Log to file without color formatting
                 if cls.logger:
@@ -116,71 +109,29 @@ class Debug:
                         cls.logger.error(message)
 
     @classmethod
+    def _log_worker(cls):
+        while True:
+            color_code, log_type, log_message, mode = cls.console_queue.get()
+            cls._process_log(color_code, log_type, log_message, mode)
+
+    @classmethod
+    def _process_log(cls, color_code, log_type, log_message, mode):
+        if mode == LogMode.UPDATE:
+            sys.stdout.write('\r')
+            sys.stdout.write(f"{color_code}{log_message}{cls.ANSI_CODES['reset']}")
+            sys.stdout.flush()
+            cls.last_message = LogMode.UPDATE
+        elif mode == LogMode.NEWLINE:
+            if cls.last_message == LogMode.UPDATE:
+                sys.stdout.write("\n")
+            sys.stdout.write(f"{color_code}{log_message}{cls.ANSI_CODES['reset']}\n")
+            sys.stdout.flush()
+            cls.last_message = LogMode.NEWLINE
+
+    @classmethod
     def shutdown(cls):
         if cls.listener:
             cls.listener.stop()
-
-# import threading
-# import traceback
-# import datetime
-# import sys
-# from enum import Enum
-#
-#
-# class LogMode(Enum):
-#     UPDATE = "update"
-#     NEWLINE = "newline"
-#
-#
-# class Debug:
-#     # Determine if logging is enabled based on the environment
-#     # enabled = os.getenv('PYTHON_ENV') in ['staging', 'testing', 'development']
-#     enabled = True
-#     # Mutex for thread-safe logging
-#     mutex = threading.Lock()
-#     # Prefix for all log messages
-#     prefix = ""
-#     last_message = LogMode.NEWLINE
-#
-#     # ANSI escape codes for coloring the console output
-#     ANSI_CODES = {
-#         'red': "\033[31m",
-#         'green': "\033[32m",
-#         'yellow': "\033[33m",
-#         'reset': "\033[0m",
-#     }
-#
-#     @classmethod
-#     def info(cls, *messages, mode=LogMode.NEWLINE, force=False):
-#         cls._log(cls.ANSI_CODES['green'], "INFO", *messages, mode=mode, force=force)
-#
-#     @classmethod
-#     def warn(cls, *messages, mode=LogMode.NEWLINE, force=False):
-#         cls._log(cls.ANSI_CODES['yellow'], "WARN", *messages, mode=mode, force=force)
-#
-#     @classmethod
-#     def error(cls, *messages, mode=LogMode.NEWLINE, force=False):
-#         cls._log(cls.ANSI_CODES['red'], "ERROR", *messages, mode=mode, force=force)
-#
-#     @classmethod
-#     def _log(cls, color_code, log_type, *messages, mode=LogMode.NEWLINE, force=False):
-#         if cls.enabled or force:
-#             with cls.mutex:
-#                 timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#                 for message in messages:
-#                     if isinstance(message, Exception) and mode == LogMode.NEWLINE:
-#                         message = f"{message}\n{traceback.format_exc()}"
-#
-#                     if mode == LogMode.UPDATE:
-#                         sys.stdout.write('\r')
-#                         sys.stdout.write(
-#                             f"{color_code}[{timestamp}] {cls.prefix} {log_type}: {message}{cls.ANSI_CODES['reset']}")
-#                         sys.stdout.flush()
-#                         cls.last_message = LogMode.UPDATE
-#                     elif mode == LogMode.NEWLINE:
-#                         if cls.last_message == LogMode.UPDATE:
-#                             sys.stdout.write("\n")
-#                         sys.stdout.write(
-#                             f"{color_code}[{timestamp}] {cls.prefix} {log_type}: {message}{cls.ANSI_CODES['reset']}\n")
-#                         sys.stdout.flush()
-#                         cls.last_message = LogMode.NEWLINE
+        if cls.worker_thread:
+            cls.console_queue.put((None, None, None, None))  # Signal to stop the worker thread
+            cls.worker_thread.join()
